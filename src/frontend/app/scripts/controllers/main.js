202f3841d784caf7e25ab3c22c729c61854ef6fd
JihadMotii-REISys <jihad.motii@reisystems.com>
2015-11-13 13:56:43 -0500

    demo MAP

--- src/frontend/app/scripts/controllers/main.js
+++ src/frontend/app/scripts/controllers/main.js
@@ -14,16 +14,76 @@ app.controller('MainCtrl', ['$scope', 'ApiInterfaceService', function ($scope, A
       'Karma'
     ];
 
-    ApiInterfaceService.call('noaa', '', {'x': 1}).then(
+    ApiInterfaceService.call('noaa', '', {}).then(
     function(xmlData){
         var x2js = new X2JS();
         var jsonData = x2js.xml_str2json( xmlData );
-        console.log(xmlData);
-        console.log(jsonData);
+//        console.log(xmlData);
+//        console.log(jsonData);
     },
     function(error) {
         console.log(error);
     });
 
+    var map = new Datamap({
+        element: document.getElementById('container'),
+        scope: 'usa',
+        geographyConfig: {
+            highlightBorderColor: '#bada55',
+            popupTemplate: function(geography, data) {
+                console.log(geography);
+              return '<div class="hoverinfo">' + geography.properties.name +'</div>'
+            },
+//            highlightBorderWidth: 3
+        },
+        fills: {
+            'HIGH': '#CC4731',
+            'MEDIUM': '#306596',
+            'LOW': '#667FAF',
+            'defaultFill': '#DDDDDD'
+        },
+        data: {
+            "AZ": {
+              "fillKey": "MEDIUM",
+            },
+            "CO": {
+              "fillKey": "HIGH",
+            },
+            "DE": {
+              "fillKey": "LOW",
+            },
+            "GA": {
+              "fillKey": "MEDIUM",
+            }
+        }
+    });
+    
+    var bombs = [{
+        name: 'Joe 4',
+        radius: 4,
+        yield: 400,
+        country: 'USSR',
+        fillKey: 'RUS',
+        significance: 'First fusion weapon test by the USSR (not "staged")',
+        date: '1953-08-12',
+        latitude: 37.7680,
+        longitude: -78.2057
+      },
+    ];
+    //draw bubbles for bombs
+    map.bubbles(bombs, {
+        popupTemplate: function (geo, data) { 
+                return ['<div class="hoverinfo">' +  data.name,
+                '<br/>Payload: ' +  data.yield + ' kilotons',
+                '<br/>Country: ' +  data.country + '',
+                '<br/>Date: ' +  data.date + '',
+                '</div>'].join('');
+        }
+    });
+    
+    //draw a legend for this map
+    map.labels();
+    map.legend();
+    
     $scope.test = 'Hey !!';
   }]);
--- src/frontend/test/karma.conf.js
+++ src/frontend/test/karma.conf.js
@@ -35,6 +35,9 @@ module.exports = function(config) {
       'bower_components/angularfire/dist/angularfire.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/x2js/xml2json.min.js',
+      'bower_components/d3/d3.js',
+      'bower_components/topojson/topojson.js',
+      'bower_components/datamaps/dist/datamaps.all.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
