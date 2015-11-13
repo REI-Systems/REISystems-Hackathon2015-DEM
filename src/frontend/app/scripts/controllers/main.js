6bbaff889659b8c73d97ee2a3261ba1dc85948ee
JihadMotii-REISys <jihad.motii@reisystems.com>
2015-11-13 14:48:18 -0500

    implementing async ajax call multiple APIs

--- src/frontend/app/scripts/controllers/main.js
+++ src/frontend/app/scripts/controllers/main.js
@@ -14,76 +14,99 @@ app.controller('MainCtrl', ['$scope', 'ApiInterfaceService', function ($scope, A
       'Karma'
     ];
 
-    ApiInterfaceService.call('noaa', '', {}).then(
-    function(xmlData){
-        var x2js = new X2JS();
-        var jsonData = x2js.xml_str2json( xmlData );
+    var apiCalls = [
+        {
+            "name": "noaa",
+            "oParams": {
+                "x":1
+            }
+        },
+        {
+            "name": "femaDisaster",
+            "oParams": {
+                "$filter": "declarationDate gt '2015-07-01T04:00:00.000z'",
+                "$top": "1000",
+                "$format": "json"
+            }
+        },
+        {
+            "name": "usGeoloc",
+            "oParams": {}
+        }
+    ];
+
+    ApiInterfaceService.calls(apiCalls).then(
+    function(resultls){
+        console.log(resultls);
+//        var x2js = new X2JS();
+//        var jsonData = x2js.xml_str2json( xmlData );
 //        console.log(xmlData);
 //        console.log(jsonData);
-    },
-    function(error) {
-        console.log(error);
-    });
 
-    var map = new Datamap({
-        element: document.getElementById('container'),
-        scope: 'usa',
-        geographyConfig: {
-            highlightBorderColor: '#bada55',
-            popupTemplate: function(geography, data) {
-                console.log(geography);
-              return '<div class="hoverinfo">' + geography.properties.name +'</div>'
+        var map = new Datamap({
+            element: document.getElementById('container'),
+            scope: 'usa',
+            geographyConfig: {
+                highlightBorderColor: '#bada55',
+                popupTemplate: function(geography, data) {
+                    console.log(geography);
+                  return '<div class="hoverinfo">' + geography.properties.name +'</div>'
+                },
+    //            highlightBorderWidth: 3
             },
-//            highlightBorderWidth: 3
-        },
-        fills: {
-            'HIGH': '#CC4731',
-            'MEDIUM': '#306596',
-            'LOW': '#667FAF',
-            'defaultFill': '#DDDDDD'
-        },
-        data: {
-            "AZ": {
-              "fillKey": "MEDIUM",
+            fills: {
+                'HIGH': '#CC4731',
+                'MEDIUM': '#306596',
+                'LOW': '#667FAF',
+                'defaultFill': '#DDDDDD'
             },
-            "CO": {
-              "fillKey": "HIGH",
-            },
-            "DE": {
-              "fillKey": "LOW",
-            },
-            "GA": {
-              "fillKey": "MEDIUM",
+            data: {
+                "AZ": {
+                  "fillKey": "MEDIUM",
+                },
+                "CO": {
+                  "fillKey": "HIGH",
+                },
+                "DE": {
+                  "fillKey": "LOW",
+                },
+                "GA": {
+                  "fillKey": "MEDIUM",
+                }
             }
-        }
-    });
-    
-    var bombs = [{
-        name: 'Joe 4',
-        radius: 4,
-        yield: 400,
-        country: 'USSR',
-        fillKey: 'RUS',
-        significance: 'First fusion weapon test by the USSR (not "staged")',
-        date: '1953-08-12',
-        latitude: 37.7680,
-        longitude: -78.2057
-      },
-    ];
-    //draw bubbles for bombs
-    map.bubbles(bombs, {
-        popupTemplate: function (geo, data) { 
+        });
+
+        var bombs = [{
+            name: 'Joe 4',
+            radius: 4,
+            yield: 400,
+            country: 'USSR',
+            fillKey: 'RUS',
+            significance: 'First fusion weapon test by the USSR (not "staged")',
+            date: '1953-08-12',
+            latitude: 37.7680,
+            longitude: -78.2057
+          }
+        ];
+        //draw bubbles for bombs
+        map.bubbles(bombs, {
+            popupTemplate: function (geo, data) { 
                 return ['<div class="hoverinfo">' +  data.name,
                 '<br/>Payload: ' +  data.yield + ' kilotons',
                 '<br/>Country: ' +  data.country + '',
                 '<br/>Date: ' +  data.date + '',
                 '</div>'].join('');
-        }
+            }
+        });
+
+        //draw a legend for this map
+        map.labels();
+        map.legend();
+    },
+    function(error) {
+        console.log(error);
     });
-    
-    //draw a legend for this map
-    map.labels();
-    map.legend();
-    
+
+
     $scope.test = 'Hey !!';
-  }]);
+}]);
--- src/frontend/app/scripts/services/ApiInterfaceService.js
+++ src/frontend/app/scripts/services/ApiInterfaceService.js
@@ -11,6 +11,8 @@ app.service('ApiInterfaceService', ['$http', '$q', '$log', function ($http, $q, 
     //list of APIs we're using
     var APIs = {
         "noaa": "https://cx0kmi7urj.execute-api.us-east-1.amazonaws.com/prod/forecasts",
+        "usGeoloc": "/data/us-geoloc.json",
+        "femaDisaster": "http://www.fema.gov/api/open/v1/DisasterDeclarationsSummaries"
     };
 
     this.APIs = APIs;
@@ -27,12 +29,42 @@ app.service('ApiInterfaceService', ['$http', '$q', '$log', function ($http, $q, 
         var deferred = $q.defer();
 
         $http.get(APIs[ApiName] + ApiSuffix, {params: oParams})
-            .success(function(data) { 
-                deferred.resolve(data);
-            }).error(function(msg, code) {
-                deferred.reject(msg);
-                $log.error(msg, code);
-            });
+        .success(function(data) { 
+            deferred.resolve(data);
+        }).error(function(msg, code) {
+            deferred.reject(msg);
+            $log.error(msg, code);
+        });
+
+        return deferred.promise;
+    };
+
+    /**
+     * common function to perform multiple API CALLS
+     * 
+     * @param Array object aApiParams
+     * @returns {$q@call;defer.promise}
+     */
+    this.calls = function(aApiParams) {
+        var deferred = $q.defer();
+        var urlCalls = [];
+
+        angular.forEach(aApiParams, function(oApiParam){
+            urlCalls.push($http.get(APIs[oApiParam.name], {params:oApiParam.oParams}));
+        });
+
+        $q.all(urlCalls)
+        .then(
+            function(results) {
+                deferred.resolve(results);
+            },
+            function(errors) {
+                deferred.reject(errors);
+            },
+            function(updates) {
+                deferred.update(updates);
+            }
+        );
 
         return deferred.promise;
     };
