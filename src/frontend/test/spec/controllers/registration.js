db852dd2040c381be075101f0f3228c23c4ec0c2
Diego Ruiz <diego.ruiz@reisystems.com>
2015-11-15 18:55:24 +0000

    DEM-12 #comment refactoring for code cleaniness and added some unit tests

--- src/frontend/test/spec/controllers/registration.js
+++ src/frontend/test/spec/controllers/registration.js
@@ -11,11 +11,47 @@ describe('Controller: RegistrationCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('RegistrationCtrl', {
+    RegistrationCtrl = $controller('RegistrationCtrl', {
       $scope: scope
-      // place here mocked dependencies
     });
   }));
 
-  
+
+  //specs
+  describe('static date tests', function(){
+    it('should have disaster interest options',function(){
+      var opt = scope.disasterInterestOptions.length > 0;
+      expect(opt).toBeGreaterThan(0);
+    });
+    it('should have assistance interest options',function(){
+      var opt = scope.assistanceInterestOptions.length > 0;
+      expect(opt).toBeGreaterThan(0);
+    });
+    it('should have service interest options',function(){
+      var opt = scope.serviceInterestOptions.length > 0;
+      expect(opt).toBeGreaterThan(0);
+    });
+    it('should have compensation type options',function(){
+      var opt = scope.compensationTypeOptions.length > 0;
+      expect(opt).toBeGreaterThan(0);
+    });
+  });
+  describe("async tests", function(){
+    it('should get the state options',function(){
+      scope.loadStateOptions();
+      setTimeout(function() {
+        var opt = scope.stateOptions.length > 0;
+        expect(opt).toBeGreaterThan(0);
+        done();
+      }, 3000);
+    });
+    it('should get the past disaster options',function(){
+      scope.loadPastDisasterOptions();
+      setTimeout(function() {
+        var opt = scope.disasterParticipationOptions.length > 0;
+        expect(opt).toBeGreaterThan(0);
+        done();
+      }, 3000);
+    });
+  });
 });
