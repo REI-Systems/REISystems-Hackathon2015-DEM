05ebfbac6a83d0fa568d1ffddb45bc9803492701
CarlosValle <REI Systems>
2015-11-15 02:04:14 -0500

    Homepage theming

--- src/frontend/app/scripts/app.js
+++ src/frontend/app/scripts/app.js
@@ -44,3 +44,52 @@ var app = angular
         redirectTo: '/'
       });
   }]);
+
+
+  angular.module('frontendApp')
+    .controller('NavCtrl', function ($scope, $timeout, $mdSidenav, $mdDialog) {
+      $scope.toggleLeft = buildDelayedToggler('left');
+      /**
+       * Navigate to url
+       */
+      $scope.navigateTo = function(to, event){
+        $mdDialog.show(
+          $mdDialog.alert()
+          .title('Navigating')
+          .content('Imagine being taken to ' + to)
+          .ariaLabel('Navigation demo')
+          .ok('Ok')
+          .targetEvent(event)
+        );
+      };
+      /**
+       * Supplies a function that will continue to operate until the
+       * time is up.
+       */
+      function debounce(func, wait, context) {
+        var timer;
+        return function debounced() {
+          var context = $scope,
+              args = Array.prototype.slice.call(arguments);
+          $timeout.cancel(timer);
+          timer = $timeout(function() {
+            timer = undefined;
+            func.apply(context, args);
+          }, wait || 10);
+        };
+      }
+      /**
+       * Build handler to open/close a SideNav
+       */
+      function buildDelayedToggler(navID) {
+        return debounce(function() {
+          $mdSidenav(navID).toggle();
+        }, 200);
+      }
+
+    })
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav) {
+      $scope.close = function () {
+        $mdSidenav('left').close();
+      };
+    });
